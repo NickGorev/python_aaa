5f0c1a36815a4808ff51ebf25efbfca0b7ae95cf
NickGorev <gnamail62@gmail.com>
2022-10-11 09:31:39 +0300

    ver1

--- count_vectorizer/count_vectorizer.py
+++ count_vectorizer/count_vectorizer.py
@@ -1,5 +1,4 @@
 from typing import List
-from collections import defaultdict
 
 
 class CountVectorizer:
@@ -7,7 +6,7 @@ class CountVectorizer:
     def __init__(self, lowercase: bool = True):
         """lowercase: bool - нужно ли переводить строки в нижний регистр"""
         self.lowercase = lowercase
-        self._vocabulary = None
+        self._vocabulary = {}
 
     def _preproc(self, corpus: List[str]) -> List[List[str]]:
         """Предобработка списка строк:
@@ -18,22 +17,24 @@ class CountVectorizer:
             processed.append(sentence.split())
         return processed
 
+    def _build_vicabulary(self, processed_corpus: List[List[str]]) -> None:
+        """Построение словаря self._vocabulary
+        ключ - слово, значение индекс слова в терм-документной матрице"""
+        self._vocabulary = {}
+        for words_arr in processed_corpus:
+            for word in words_arr:
+                if word not in self._vocabulary:
+                    self._vocabulary[word] = len(self._vocabulary)
+
     def fit_transform(self, corpus: List[str]) -> List[List[int]]:
         """Вычисление терм-документной матрицы"""
-
         processed_corpus = self._preproc(corpus)
+        self._build_vicabulary(processed_corpus)
 
-        n_sentences = len(corpus)
-        self._vocabulary = defaultdict(lambda: [0] * n_sentences)
+        ans = [[0] * len(self._vocabulary) for _ in range(len(corpus))]
         for i, words_arr in enumerate(processed_corpus):
             for word in words_arr:
-                self._vocabulary[word][i] += 1
-
-        n_words = len(self._vocabulary)
-        ans = [[0] * n_words for _ in range(n_sentences)]
-        for i, freques in enumerate(self._vocabulary.values()):
-            for j in range(n_sentences):
-                ans[j][i] = freques[j]
+                ans[i][self._vocabulary[word]] += 1
         return ans
 
     def get_feature_names(self) -> List[str]:
